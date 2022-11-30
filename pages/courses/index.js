612d351e7305268cded2e329d2431a3cd1413aaf
Aseef Ahmed <aahmed@linz.govt.nz>
2022-12-01 01:37:51 +1300

    changes

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,91 +1,177 @@
-import React, {useEffect, useState} from 'react'
-import PageHeader from '../../shared/layout-components/page-header/page-header'
-import { Modal, Button, Breadcrumb, Row, Col, Card } from "react-bootstrap";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import Link from "next/link"
-import Seo from '../../shared/layout-components/seo/seo';
-import dynamic from 'next/dynamic';
-const FromInlineEditEditor = dynamic(()=>import('../../shared/data/forms/formEditers/FromInlineEditEditor'), { ssr: false })
-const Modalineditor = dynamic(()=>import('../../shared/data/forms/formEditers/Modalineditor'), { ssr: false })
-const Wrappers = dynamic(()=>import('../../shared/data/forms/formEditers/Wrappers'), { ssr: false })
+import React, { Fragment, useState } from "react";
+import {
+  Breadcrumb,
+  Button,
+  Card,
+  Col,
+  Dropdown,
+  Modal,
+  Row,
+} from "react-bootstrap";
+import PageHeader from "../../shared/layout-components/page-header/page-header";
+import Link from "next/link";
+import {
+  Singleselectday,
+  SingleselectMonth,
+  Singleselectyear,
+} from "../../shared/data/forms/formlayouts";
+import Seo from "../../shared/layout-components/seo/seo";
+const FormLayouts = () => {
+  const [course, setCourse] = useState("");
+  const [price, setPrice] = useState("");
+  const [duration, setDuration] = useState("");
+  const [description, setDescription] = useState("");
+  const [requirements, setRequirements] = useState("");
+  const [learning_outcome, setLearningOutcome] = useState("");
+  const [who_this_course_is_for, setWhoThisCourseIsFor] = useState("");
 
-const WysiwygEditer = () => {
-	// Modal
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
-  let Spk
+  const submitCourse = async () => {
+    const response = await fetch(
+      "https://499nkl85l0.execute-api.ap-southeast-2.amazonaws.com/prod/blogs",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          course,
+          price,
+          duration,
+          description,
+          requirements,
+          learning_outcome,
+          who_this_course_is_for,
+        }),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-
-
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div>
-      <Seo title="Courses"/>
+      <Seo title="Form Layouts" />
 
-      <PageHeader title="Courses" item="Forms" active_item="Courses"/>
-
-      <Row className="row-sm">
-        <Col md={12} lg={12}>
-          <Card className="custom-card">
-            <Card.Header>
-              <h6 className="main-content-label mb-1"> From Inline-Edit Editor</h6>
-              <p className="text-muted card-sub-title">
-                From Inline-Edit Editor
-              </p>
-            </Card.Header>
-            <Card.Body>
-              <FromInlineEditEditor />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-
-      <Row className="row-sm">
-        <Col md={12} lg={12}>
-          <Card className="custom-card">
+      <PageHeader
+        title="Form Layouts"
+        item="Forms"
+        active_item="Form Layouts"
+      />
+      {/* <!-- Row --> */}
+      <Row className=" row-sm">
+        <Col lg={6} md={12}>
+          <Card className=" custom-card">
             <Card.Body>
               <div>
-                <h6 className="main-content-label mb-1">
-                  View Editor in Modal
-                </h6>
+                <h6 className="main-content-label mb-1">Horizonatal Form</h6>
                 <p className="text-muted card-sub-title">
-                  View text editor that wrap inside a modal.
+                  A form control layout using basic layout.
                 </p>
               </div>
-              <Button
-                variant="primary" onClick={handleShow}>
-                View Live Demo
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <Modalineditor />
-                </Modal.Body>
-              </Modal>
+              <div className="">
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Course Title</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <input
+                      className="form-control"
+                      type="text"
+                      onChange={(e) => setCourse(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Price</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <input
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setPrice(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Duration</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <input
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setDuration(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Description</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <textarea
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Requirements</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <textarea
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setRequirements(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">What You Will Learn</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <textarea
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setLearningOutcome(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row row-xs align-items-center mg-b-20">
+                  <div className="col-md-4">
+                    <label className="mg-b-0">Who this course for</label>
+                  </div>
+                  <div className="col-md-8 mg-t-5 mg-md-t-0">
+                    <textarea
+                      className="form-control"
+                      type="number"
+                      onChange={(e) => setWhoThisCourseIsFor(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="form-group row justify-content-end mb-0">
+                  <div className="col-md-8 ps-md-2 btn-list">
+                    <Button
+                      className="btn ripple btn-primary pd-x-30 mg-r-5"
+                      onClick={submitCourse}
+                    >
+                      Create Course
+                    </Button>
+                  </div>
+                </div>
+              </div>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-      <Row className="row-sm">
-        <Col md={12} lg={12}>
-          <Card className="custom-card">
-            <Card.Header>
-              <h6 className="main-content-label mb-1">Summernote Editor</h6>
-              <p className="text-muted card-sub-title">
-                Summernote Editor
-              </p>
-            </Card.Header>
-            <Card.Body>
-              <Wrappers />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      </div>
-  )
-}
-WysiwygEditer.layout = "Contentlayout"
+      {/* <!-- End Row --> */}
+    </div>
+  );
+};
+FormLayouts.layout = "Contentlayout";
 
-export default WysiwygEditer
+export default FormLayouts;
