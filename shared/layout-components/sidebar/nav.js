5f1607b4bf64767a9d2050ab1d1be7b50983644a
Aseef Ahmed <aahmed@linz.govt.nz>
2022-11-30 15:43:39 +1300

    changes

---
+++ shared/layout-components/sidebar/nav.js
@@ -0,0 +1,23 @@
+export const MENUITEMS = [
+  {
+    menutitle: "DASHBOARD",
+    Items: [
+      {
+        path: "/blogs",
+        icon: "ti-home",
+        type: "link",
+        active: false,
+        selected: false,
+        title: "Blogs",
+      },
+      {
+        path: "/courses",
+        icon: "ti-home",
+        type: "link",
+        active: false,
+        selected: false,
+        title: "Courses",
+      },
+    ],
+  },
+];
